6b0881b3751cece0121765cbca3b4a68b26c52b4
youngfromnowhere <graph1000limited@gmail.com>
2023-05-22 19:36:43 +0900

    Initialize

---
+++ src/main/java/com/cmjc/batchscheduler/BatchSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.cmjc.batchscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchSchedulerApplication.class, args);
+	}
+
+}
